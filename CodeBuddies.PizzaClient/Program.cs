6100a887536231622e29ee1f844ba759b163f220
ShenanPerera <shenaperera2001@gmail.com>
2024-02-08 14:59:21 +0530

    Product functionalty

--- CodeBuddies.PizzaClient/Program.cs
+++ CodeBuddies.PizzaClient/Program.cs
@@ -1,10 +1,13 @@
 using CodeBuddies.PizzaClient;
+using CodeBuddies.PizzaClient.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<IProductService, ProductService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
